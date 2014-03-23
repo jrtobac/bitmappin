086986103e6da0caa22a495407846e180aabf1da
Jeremy <jeremy@jeremy.(none)>
2014-03-23 13:04:44 -0700

    check extension

--- bitmappin.c
+++ bitmappin.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* global variables for input and output file names */
 char *g_infile, *g_outfile;
@@ -7,12 +8,12 @@ char *g_infile, *g_outfile;
 /* used to parse the command line input */
 int parse_input(int argc, char *argv[])
 {
+#define FILE_EXTENSION ".bmp"
 	extern char *optarg;
-
 	int c, err = 0;
 	char iflag = 0, oflag = 0;
-
-	static char usage[] = "usage: %s -i infile -o outfile\n";
+	char *in_extension, *out_extension;
+	static char usage[] = "usage: %s -i infile.bmp -o outfile.bmp\n";
 
 	/* parse the command line arguments -i for input -o for output */
 	while((c = getopt(argc, argv, "i:o:")) != -1){
@@ -28,14 +29,27 @@ int parse_input(int argc, char *argv[])
 			err = 1;
 		}	
 	}
-
+	
 	/* check for command line errors */
-	if(iflag == 0){
+	if(argc < 5){
+		fprintf(stderr, "%s too few arguments\n", argv[0]);
+		fprintf(stderr, usage, argv[0]);
+		exit(1);
+	}
+	else if(iflag == 0){
 		fprintf(stderr, "%s: missing -i option\n", argv[0]);
 		fprintf(stderr, usage, argv[0]);
 		exit(1);
 	}else if(oflag == 0){
 		fprintf(stderr, "%s: missing -o option\n", argv[0]);
+		fprintf(stderr, usage, argv[0]);
+		exit(1);
+	}else if ((in_extension = strchr(g_infile, '.')) == NULL || (strcmp(in_extension, FILE_EXTENSION) != 0 )){
+		fprintf(stderr, "%s: incorrect input file extension\n", argv[0]);
+		fprintf(stderr, usage, argv[0]);
+		exit(1);
+	}else if ((out_extension = strchr(g_outfile, '.')) == NULL || (strcmp(out_extension, FILE_EXTENSION) != 0)){
+		fprintf(stderr, "%s: incorrect output file extension\n", argv[0]); 	
 		fprintf(stderr, usage, argv[0]);
 		exit(1);
 	}else if(err){
