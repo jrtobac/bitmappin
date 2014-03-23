2d593cb4926504a4c13e84aebe581825a1cda0e9
Evan Tobac <evantobac@gmail.com>
2014-03-22 20:54:14 -0700

    Added basic structure so we can get started.

--- bitmappin.c
+++ bitmappin.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
-int main(){
 
-return 0;
+int parse_input(int argc, char *argv[])
+{
+	return 0;
+}
+
+int read_in_file()
+{
+
+
+	return 0;
+}
+
+int main(int argc, char *argv[])
+{
+
+	parse_input(argc, argv);
+	read_in_file();
+	return 0;
 }
